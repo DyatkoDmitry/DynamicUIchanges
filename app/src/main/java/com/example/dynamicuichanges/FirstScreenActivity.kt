c466602d4835a229f89708d0e2014c0b42d20df8
Dyatko Dmitry <DyatkoDmitry@gmail.com>
2024-03-10 22:27:08 +0300

    init: start init commit

---
+++ app/src/main/java/com/example/dynamicuichanges/FirstScreenActivity.kt
@@ -0,0 +1,52 @@
+package com.example.dynamicuichanges
+
+import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.dynamicuichanges.databinding.ActivityFirstScreenBinding
+
+class FirstScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFirstScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityFirstScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setFirstScreenImage()
+
+        setButtonListeners()
+    }
+
+    private fun setFirstScreenImage(){
+        Glide.with(this)
+            .load(AppConstants.url)
+            .centerCrop()
+            .into(binding.FirstScreenImage)
+    }
+
+    private fun setButtonListeners(){
+        binding.hide.setOnClickListener {
+
+            binding.FirstScreenImage.animate().apply {
+                alpha(0f)
+                setDuration(1000)
+                setInterpolator(AccelerateDecelerateInterpolator())
+            }.start()
+
+        }
+
+        binding.show.setOnClickListener {
+
+            binding.FirstScreenImage.animate().apply {
+                alpha(1f)
+                setDuration(1000)
+                setInterpolator(AccelerateDecelerateInterpolator())
+            }.start()
+
+        }
+    }
+}
